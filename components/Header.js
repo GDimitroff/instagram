aeb8c8e89ac4a76883fc601d89d63190adf9242e
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2022-11-13 18:12:08 +0200

    Add middle part of the header

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,18 @@
 import Image from 'next/image';
+import {
+  MagnifyingGlassIcon,
+  PlusCircleIcon,
+  HeartIcon,
+  PaperAirplaneIcon,
+  MenuIcon,
+} from '@heroicons/react/24/solid';
 
 function Header() {
   return (
-    <div className="">
-      {/* Left - Logo */}
+    <div>
       <div className="flex justify-between bg-white max-w-6xl">
-        <div className="relative hidden lg:inline-grid h-24 w-24 cursor-pointer">
+        {/* Left - Logo */}
+        <div className="relative hidden lg:inline-grid w-24 cursor-pointer">
           <Image
             src="https://links.papareact.com/ocw"
             fill
@@ -13,7 +20,7 @@ function Header() {
             alt="Instagram Logo"
           />
         </div>
-        <div className="relative w-10 h-10 lg:hidden flex-shrink-0 cursor-pointer">
+        <div className="relative w-10 lg:hidden flex-shrink-0 cursor-pointer">
           <Image
             src="https://links.papareact.com/jjm"
             fill
@@ -21,6 +28,22 @@ function Header() {
             alt="Instagram Logo"
           />
         </div>
+
+        {/* Middle - Search input field */}
+        <div className="max-w-xs">
+          <div className="relative mt-1 p-3 rounded-md">
+            <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
+              <MagnifyingGlassIcon className="h-5 w-5 text-gray-500" />
+            </div>
+            <input
+              className="bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 focus:ring-black focus:border-black rounded-md"
+              type="text"
+              placeholder="Search"
+            />
+          </div>
+        </div>
+
+        {/* Right */}
       </div>
     </div>
   );
